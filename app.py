bc0a9d73b720fd9a473d1c516030568f0994c29a
gonzalezbri <4717bg@gmail.com>
2023-09-14 00:03:15 -0700

    feedback register

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from backend.models import db  # Import db from your models module
 from backend.events import event_blueprint  # Import the event Blueprint
 from backend.users import user_blueprint  # Import the user Blueprint
 from backend.guests import guest_blueprint  # Import the guest Blueprint
+from backend.feedback import feedback_blueprint
 
 app = Flask(__name__)
 
@@ -22,6 +23,7 @@ migrate = Migrate(app, db)
 app.register_blueprint(event_blueprint, url_prefix='/api/event')
 app.register_blueprint(user_blueprint, url_prefix='/api/user')
 app.register_blueprint(guest_blueprint, url_prefix='/api/guest')
+app.register_blueprint(feedback_blueprint, url_prefix='/api/feedback')
 
 
 
